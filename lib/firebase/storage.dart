77b85ce0cd502cf456fbf750b2ba574ad3e09f17
Ahmadu-Suleiman <ahmadumeta4.1@gmail.com>
2024-08-28 13:16:18 +0100

    add firebase plugins

---
+++ lib/firebase/storage.dart
@@ -0,0 +1,11 @@
+class Storage {
+  // static const Uuid uuid = Uuid();
+  // static final _storageRef = FirebaseStorage.instance.ref();
+  //
+  // static Future<String> uploadProfileImage(String uid, File file) async {
+  //   final fileName = uid;
+  //   final imageRef = _profileImagesRef.child(fileName);
+  //   await imageRef.putFile(file);
+  //   return await imageRef.getDownloadURL();
+  // }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:product_catalog_app/home_page.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
